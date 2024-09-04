100b6dd70ba524eba3bb0e85f013a025adb4df8f
Servet Isikli <servetisikli@gmail.com>
2024-09-04 14:33:18 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import { CardFooter, ChakraProvider } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <ChakraProvider>
+        <Navbar />
+        <Home />
+        <Footer />
+      </ChakraProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Carousel from "../components/Carousel";
+
+const Home = () => {
+  return (
+    <div>
+      Home Page-Store-
+      <Carousel />
+    </div>
+  );
+};
+
+export default Home;
