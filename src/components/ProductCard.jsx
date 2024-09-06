28ce44e1b57f7673a4465f3e3b37f552cf65d994
Servet Isikli <servetisikli@gmail.com>
2024-09-06 09:21:54 +0200

    ProductCard updated

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,154 @@
-import React from 'react'
+import React from "react";
+import {
+  Flex,
+  Box,
+  Image,
+  Badge,
+  useColorModeValue,
+  Icon,
+  chakra,
+  Tooltip,
+} from "@chakra-ui/react";
+import { BsStar, BsStarFill, BsStarHalf } from "react-icons/bs";
+import { FiShoppingCart } from "react-icons/fi";
 
-const ProductCard = () => {
+function Rating({ rating, numReviews }) {
   return (
-    <div>ProductCard</div>
-  )
+    <Box display="flex" alignItems="center">
+      {Array(5)
+        .fill("")
+        .map((_, i) => {
+          const roundedRating = Math.round(rating * 2) / 2;
+          if (roundedRating - i >= 1) {
+            return (
+              <BsStarFill
+                key={i}
+                style={{ marginLeft: "1" }}
+                color={i < rating ? "teal.500" : "gray.300"}
+              />
+            );
+          }
+          if (roundedRating - i === 0.5) {
+            return <BsStarHalf key={i} style={{ marginLeft: "1" }} />;
+          }
+          return <BsStar key={i} style={{ marginLeft: "1" }} />;
+        })}
+      <Box as="span" ml="2" color="gray.600" fontSize="sm">
+        {numReviews} review{numReviews > 1 && "s"}
+      </Box>
+    </Box>
+  );
 }
 
-export default ProductCard
+function ProductCard({ product }) {
+  return (
+    <Flex p={50} w="full" alignItems="center" justifyContent="center">
+      <Box
+        bg={useColorModeValue("white", "gray.800")}
+        maxW="sm"
+        borderWidth="1px"
+        rounded="lg"
+        shadow="lg"
+        position="relative"
+      >
+
+
+
+
+
+        {/* 1. Add a badge to the top right corner of the product card that says
+        "Top Rated" if the product rating is greater than or equal to 4.5. */} 
+      
+        {product.rating.rate >= 4.5 && (
+          <Badge
+            rounded="full"
+            px="2"
+            fontSize="0.8em"
+            colorScheme="green"
+            position="absolute"
+            top={2}
+            right={2}
+          >
+            Top Rated
+          </Badge>
+        )}
+
+
+
+
+
+
+
+
+        {/* 2. Image Pages resized */} 
+
+
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <Image
+            src={product.image}
+            alt={`Picture of ${product.title}`}
+            roundedTop="lg"
+            boxSize="300px"
+            objectFit="contain"
+          />
+        </Box>
+
+
+
+
+
+
+
+        <Box p="6">
+          <Box display="flex" alignItems="baseline">
+            {product.category && (
+              <Badge rounded="full" px="2" fontSize="0.8em" colorScheme="blue">
+                {product.category}
+              </Badge>
+            )}
+
+
+            
+          </Box>
+          <Flex mt="1" justifyContent="space-between" alignContent="center">
+            <Box
+              fontSize="2xl"
+              fontWeight="semibold"
+              as="h4"
+              lineHeight="tight"
+              isTruncated
+            >
+              {product.title}
+            </Box>
+            <Tooltip
+              label="Add to cart"
+              bg="white"
+              placement={"top"}
+              color={"gray.800"}
+              fontSize={"1.2em"}
+            >
+              <chakra.a href={"#"} display={"flex"}>
+                <Icon as={FiShoppingCart} h={7} w={7} alignSelf={"center"} />
+              </chakra.a>
+            </Tooltip>
+          </Flex>
+
+          <Flex justifyContent="space-between" alignContent="center">
+            <Rating
+              rating={product.rating.rate}
+              numReviews={product.rating.count}
+            />
+            <Box fontSize="2xl" color={useColorModeValue("gray.800", "white")}>
+              <Box as="span" color={"gray.600"} fontSize="lg">
+                £
+              </Box>
+              {product.price.toFixed(2)}
+            </Box>
+          </Flex>
+        </Box>
+      </Box>
+    </Flex>
+  );
+}
+
+export default ProductCard;
