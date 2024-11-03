879857d60e37653f90d18c3f69a1277f89d8de6a
servetisikli <servetisikli@gmail.com>
2024-11-03 20:29:48 +0100

    tailwindcss added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import ProductList from "../components/ProductList";
 const Home = () => {
   return (
     <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Carousel />
       <ProductList />
     </div>
